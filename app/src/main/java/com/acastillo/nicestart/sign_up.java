de2222f8305cdce3456e1e5e0bd9a2b45ed41a75
am8DAM2 <angelcastillo4703@outlook.com>
2024-10-21 13:48:46 +0200

    Se ha creado sign up y main.

--- app/src/main/java/com/acastillo/nicestart/sign_up.java
+++ app/src/main/java/com/acastillo/nicestart/sign_up.java
@@ -1,6 +1,8 @@
 package com.acastillo.nicestart;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,19 +10,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Login extends AppCompatActivity {
+public class sign_up extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-
-
+        setContentView(R.layout.activity_sign_up);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void cancel(View v){
+        Intent intent = new Intent(sign_up.this, login.class);
+        startActivity(intent);
+    }
+
+    public void signupToMain(View view) {
+        Intent intent = new Intent(sign_up.this, main.class);
+        startActivity(intent);
     }
 }
