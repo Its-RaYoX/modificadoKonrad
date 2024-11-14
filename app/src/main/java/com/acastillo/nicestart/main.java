8b42e29ca47335797f7d238e242013841e2eb5dd
am8DAM2 <angelcastillo4703@outlook.com>
2024-11-14 11:49:56 +0100

    Depurando error

--- app/src/main/java/com/acastillo/nicestart/main.java
+++ app/src/main/java/com/acastillo/nicestart/main.java
@@ -1,17 +1,21 @@
 package com.acastillo.nicestart;
 
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.widget.ImageView;
-
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.google.android.material.snackbar.Snackbar;
+
 
 public class main extends AppCompatActivity {
 
@@ -26,7 +30,71 @@ public class main extends AppCompatActivity {
             return insets;
         });
 
+        TextView ola = findViewById(R.id.ola);
+        registerForContextMenu(ola);
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.menu_appbar, menu);
+    }
 
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        final ConstraintLayout mLayout = findViewById(R.id.main);
+
+        if (id == R.id.item) {
+            Snackbar snackbar = Snackbar
+                    .make(mLayout, "Copiando", Snackbar.LENGTH_LONG)
+                    .setAction("UNDO", view -> {
+                        Snackbar snackbar1 = Snackbar.make(mLayout, "Copia cancelada", Snackbar.LENGTH_SHORT);
+                        snackbar1.show();
+                    });
+            snackbar.show();
+        } else if (id == R.id.item2) {
+            Snackbar snackbar = Snackbar
+                    .make(mLayout, "Descargando", Snackbar.LENGTH_LONG)
+                    .setAction("UNDO", view -> {
+                        Snackbar snackbar1 = Snackbar.make(mLayout, "Descarga cancelada", Snackbar.LENGTH_SHORT);
+                        snackbar1.show();
+                    });
+            snackbar.show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        final ConstraintLayout mLayout = findViewById(R.id.main);
+
+        if (id == R.id.item) {
+            Snackbar snackbar = Snackbar
+                    .make(mLayout, "Copiando", Snackbar.LENGTH_LONG)
+                    .setAction("UNDO", view -> {
+                        Snackbar snackbar1 = Snackbar.make(mLayout, "Copia cancelada", Snackbar.LENGTH_SHORT);
+                        snackbar1.show();
+                    });
+            snackbar.show();
+        } else if (id == R.id.item2) {
+            Snackbar snackbar = Snackbar
+                    .make(mLayout, "Descargando", Snackbar.LENGTH_LONG)
+                    .setAction("UNDO", view -> {
+                        Snackbar snackbar1 = Snackbar.make(mLayout, "Descarga cancelada", Snackbar.LENGTH_SHORT);
+                        snackbar1.show();
+                    });
+            snackbar.show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_appbar, menu);
+        return true;
+    }
 }
